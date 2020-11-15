166cce68d75b7bad5a47f9832b166229309518bd
JJMorton <jjmorton1636@gmail.com>
2020-11-15 18:23:42 +0000

    adjustment to format of stats embed

--- commands/stats.js
+++ commands/stats.js
@@ -6,9 +6,14 @@ const db = require("../db.js");
 exports.run = (client, message, args) => {
 	const userid = message.author.id;
 	db.getUser(userid).then(stats => {
-		const total = Object.values(stats).reduce((x, sum) => sum + x);
 		const embed = new Discord.MessageEmbed();
-		embed.addFields(Object.entries(stats).map(x => ({ name: x[0], value: x[1], inline: true })));
+		const total = Object.values(stats).reduce((x, sum) => sum + x);
+		const fields = Object.entries(stats).map(x => ({
+			name: x[0],
+			value: "**" + x[1] + "** use" + (x[1] === 1 ? "" : "s"),
+			inline: true
+		}));
+		embed.addFields(fields);
 		embed.setFooter(`${message.member.displayName} has typed ${total} commands in total`, message.author.displayAvatarURL());
 		embed.setColor(message.member.displayColor);
 		message.channel.send(embed);
